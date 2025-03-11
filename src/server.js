07b836a16d3c50a34ccea23549e7971a9806c678
Felipe Santos <dev.felipesantos@gmail.com>
2025-03-11 15:37:38 -0300

    feat: adjust getAll method

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import tarefaRoutes from "./routes/tarefaRoutes.js";
 const app = express();
-const port = 3000;
+const port = 4000;
 app.use(express.json());
 app.use("/tarefas", tarefaRoutes);
 app.listen(port, () => {
